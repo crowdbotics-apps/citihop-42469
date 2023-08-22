e029db2ebf3e018cdb60ec13af73ef64cb464e4d
Crowdbotics <engineering@crowdbotics.com>
2023-08-22 23:00:00 +0000

    Update store.

--- store/morningfieldAPI/api.js
+++ store/morningfieldAPI/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const morningfieldAPI = axios.create({
-  baseURL: "https://morning-field-42469.botics.co",
+  baseURL: "https://citihop-42469.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function api_docs_schema_retrieve(payload) {
